4ff66ff61e4ec4be23124abc76a28f2ae8517462
Akash Kothari <akashkothari@Akashs-MacBook-Air.local>
2026-05-17 23:34:04 +0530

    new logic

--- test2.py
+++ test2.py
@@ -266,8 +266,9 @@ def main():
         # {"id": "152130", "name": "RCB vs KKR"}
         # {"id": "152141", "name": "PBKS vs MI"}
         # {"id": "152152", "name": "LSG vs CSK"}
-        {"id": "152163", "name": "KKR vs GT"},
-        {"id": "152174", "name": "RCB vs PBKS"}
+        # {"id": "152163", "name": "KKR vs GT"},
+        # {"id": "152174", "name": "RCB vs PBKS"}
+        {"id": "152185", "name": "DC vs RR"}
     ]
 
     all_results = []
